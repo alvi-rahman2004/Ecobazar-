0e129c22bceed7f3788ddd12aad9164cd4cf1c02
alvi rahman <alvi99429@gmail.com>
2025-07-07 01:05:44 +0600

    banner

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,27 @@
-let mobileSearchIcon = document.querySelector(".mobile_search")
-let mobileSearchBody = document.querySelector("#search")
-let mobileMenuCross = document.querySelector(".mobile_menu_cross")
+$(function () {
 
+    let mobileSearchIcon = document.querySelector(".mobile_search")
+    let mobileSearchBody = document.querySelector("#search")
+    let mobileMenuCross = document.querySelector(".mobile_menu_cross")
 
-mobileSearchIcon.addEventListener("click",function(){
+    mobileSearchIcon.addEventListener("click", function () {
 
-    mobileSearchBody.classList.add("search_active")
-})
+        mobileSearchBody.classList.add("search_active")
+    })
 
-mobileMenuCross.addEventListener("click",function(){
-    mobileSearchBody.classList.remove("search_active")
+    mobileMenuCross.addEventListener("click", function () {
+        mobileSearchBody.classList.remove("search_active")
+    });
+
+    // * BANNER ITEM FOR SLIDER *
+    $('.sliders').slick({
+        dots: true,
+        slidesToShow: 1,
+        arrows: true,
+        prevArrow: `<span class="prev"><iconify-icon icon="tdesign:arrow-left" width="24" height="24"></iconify-icon></span>`,
+        nextArrow: `<span class="next"><iconify-icon icon="tdesign:arrow-right" width="24" height="24"></iconify-icon></span>`,
+        autoplay: true,
+        autoplaySpeed: 2000,
+    });
+
 })
