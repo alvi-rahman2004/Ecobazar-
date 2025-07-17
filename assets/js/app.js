96884c21fcd388f4e45b9089a2ed02bd6cb49bec
alvi rahman <alvi99429@gmail.com>
2025-07-18 02:23:42 +0600

    timer

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,73 +1,82 @@
 $(function () {
 
-    let mobileSearchIcon = document.querySelector(".mobile_search")
-    let mobileSearchBody = document.querySelector("#search")
-    let mobileMenuCross = document.querySelector(".mobile_menu_cross")
+  let mobileSearchIcon = document.querySelector(".mobile_search")
+  let mobileSearchBody = document.querySelector("#search")
+  let mobileMenuCross = document.querySelector(".mobile_menu_cross")
 
-    mobileSearchIcon.addEventListener("click", function () {
+  mobileSearchIcon.addEventListener("click", function () {
 
-        mobileSearchBody.classList.add("search_active")
-    })
+    mobileSearchBody.classList.add("search_active")
+  })
 
-    mobileMenuCross.addEventListener("click", function () {
-        mobileSearchBody.classList.remove("search_active")
-    });
+  mobileMenuCross.addEventListener("click", function () {
+    mobileSearchBody.classList.remove("search_active")
+  });
 
-    // * BANNER ITEM FOR SLIDER *
-    $('.sliders').slick({
-        dots: true,
-        slidesToShow: 1,
-        arrows: true,
-        prevArrow: `<span class="prev"><iconify-icon icon="tdesign:arrow-left" width="24" height="24"></iconify-icon></span>`,
-        nextArrow: `<span class="next"><iconify-icon icon="tdesign:arrow-right" width="24" height="24"></iconify-icon></span>`,
-        autoplay: true,
-        autoplaySpeed: 2000,
-    });
+  // * BANNER ITEM FOR SLIDER *
+  $('.sliders').slick({
+    dots: true,
+    slidesToShow: 1,
+    arrows: true,
+    prevArrow: `<span class="prev"><iconify-icon icon="tdesign:arrow-left" width="24" height="24"></iconify-icon></span>`,
+    nextArrow: `<span class="next"><iconify-icon icon="tdesign:arrow-right" width="24" height="24"></iconify-icon></span>`,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  });
 
-    // *PRODUCT *
-    $('.product_parent_slider').slick({
-        dots: true,
-        infinite: true,
-        slidesToShow: 4,
-        slidesToScroll: 1,
-        arrows: true,
-        prevArrow: `<span class="prev"><iconify-icon icon="tdesign:arrow-left" width="24" height="24"></iconify-icon></span>`,
-        nextArrow: `<span class="next"><iconify-icon icon="tdesign:arrow-right" width="24" height="24"></iconify-icon></span>`,
-         responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        infinite: true,
-        dots: true
+  // *PRODUCT *
+  $('.product_parent_slider').slick({
+    dots: true,
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    arrows: true,
+    prevArrow: `<span class="prev"><iconify-icon icon="tdesign:arrow-left" width="24" height="24"></iconify-icon></span>`,
+    nextArrow: `<span class="next"><iconify-icon icon="tdesign:arrow-right" width="24" height="24"></iconify-icon></span>`,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
       }
-    },
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1,
-      }
-    }
-  ]
-    });
-    // *PRODUCT END*
+    ]
+  });
+  // *PRODUCT END*
 
-    // * TOOLTIP *
-    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
-    const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
-    // * TOOLTIP END*
+  // * TOOLTIP *
+  const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+  const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+  // * TOOLTIP END*
 
-    // * PRODUCT START *
-    $('.category-button').categoryFilter();
-    // * PRODUCT END *
+  // * PRODUCT START *
+  $('.category-button').categoryFilter();
+  // * PRODUCT END *
+
+
+  // * TIMER *
+  $('.countDown').countdown({
+    date: '12/24/2025 23:59:59'
+  }, function () {
+    alert('Merry Christmas!');
+  });
+  // * TIMER END*
 
 })
